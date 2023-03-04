9fcff976ffff3c85d00d7c9e94ab7bce8172c642
illiatishchenko98 <102582375+illiatishchenko98@users.noreply.github.com>
2023-03-04 16:47:35 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+const btns = document.querySelectorAll('button');
+
+
+
+// console.log(btns[0].classList.add('red'));
+// console.log(btns[0].classList.item(1));
+// console.log(btns[0].classList.remove('some'));
+
+
+// btns[0].addEventListener('click', () => {
+//     if (!btns[1].classList.contains('red')) {
+//         btns[1].classList.add('red');
+//     } else {
+//         btns[1].classList.remove('red');
+//     }
+
+// });
+
